6f7d4d43640da82d84f0dd262ab537155c31b69e
Ajinath-Game <gameajinath4@gmail.com>
2025-07-01 19:34:43 +0530

    DAY-2 types of variable

---
+++ DAY-2/script.js
@@ -0,0 +1,74 @@
+//Types of variable 
+
+// (1) primitive datatype
+
+// 1 number
+
+let noOfUser = 213
+const PI = 3.14
+
+
+console.log(typeof noOfUser)
+console.log(typeof PI)
+
+
+//2 string
+
+
+let userName = "ajinath"
+let userAddress = "dahigaon she"
+
+console.log(typeof userName)
+console.log(typeof userAddress)
+
+
+//3 boolean
+
+
+let IS_NUMBER_VERITY = true
+let IS_EMAIL_VERITY = false
+
+console.log(typeof IS_NUMBER_VERITY)
+console.log(typeof IS_EMAIL_VERITY)
+
+
+//4 undefined
+
+
+
+let Name
+let Address
+
+console.log(typeof Name)
+console.log(typeof Address)
+
+
+
+//5 NULL
+
+let userId =null
+let user1Address = null
+
+console.log(typeof userId)
+console.log(typeof user1Address)
+
+
+
+
+// (2) Non-primitive datatype
+
+//1 object
+
+
+let obj = (3)
+
+console.log(typeof obj)
+
+
+
+//2 array
+
+
+let arr =[1,2,3,4,5]
+
+console.log(typeof arr)
