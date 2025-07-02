5bec7ebff5a0dfeafc2bc085701d5bca12adb6d6
Ajinath-Game <gameajinath4@gmail.com>
2025-07-02 20:06:43 +0530

    DAY-3

---
+++ DAY-3/script.js
@@ -0,0 +1,117 @@
+// Opeartors
+
+// 1 Arithmetic opeartor
+
+
+// +
+
+let a=2
+let b=3
+console.log(a+b)
+
+// -
+
+
+let c=2
+let d=3
+console.log(c-d)
+
+// *
+
+let e=2
+let f=3
+console.log(e*f)
+
+// /
+
+let g=20
+let h=2
+console.log(g/h)
+
+// %
+
+let i=10
+let j=3
+console.log(i%j)
+
+// increment and decrement
+
+
+let s=2
+
+console.log(s++)
+console.log(++s)
+console.log(s--)
+console.log(--s)
+
+
+// 2 assignment opeartor
+
+// =
+
+let k=2
+let l=3
+
+console.log(k)
+console.log(l)
+
+// +=
+
+let m=2
+let n=3
+
+console.log(m+=n)
+
+// -=
+
+let o=2
+let p=3
+
+console.log(o-=p)
+
+// *=
+
+let q=2
+let r=3
+
+console.log(q*=r)
+
+// /=
+
+let v=20
+let t=2
+console.log(v/=t)
+
+
+// 3 Comparism opeartor
+
+// ==
+
+let x=10
+let y=20
+
+console.log(x==y)
+
+// ===
+
+let x1=10
+let y1='10'
+
+console.log(x1===y1)
+
+//
+
+// !=
+
+let a1=10
+let b1=20
+
+console.log(a1!=b1)
+
+// !==
+
+let a2=10
+let b2='10'
+
+console.log(a2!==b2)
+
