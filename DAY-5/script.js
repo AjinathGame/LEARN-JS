a8045be9dfe48661ada37ecbb8d2e03fd438c690
Ajinath-Game <gameajinath4@gmail.com>
2025-07-08 20:06:55 +0530

    Conditional statement

---
+++ DAY-5/script.js
@@ -0,0 +1,44 @@
+// Conditional Statements
+
+
+// 1 if statement 
+
+
+let no = 3
+
+if(no % 2 == 0){
+    console.log(no,'is even')
+}
+
+
+
+if(no % 2 != 0){
+    console.log(no,'is odd')
+}
+
+
+if(true){
+    console.log('hello')
+}
+
+
+
+// 2 if else statement
+
+
+let number = 3
+
+if(number % 2 == 0){
+    console.log(number,'is even')
+}else{
+    console.log(number,'is odd')
+}
+
+
+let number_2 = 4
+
+if(number_2 > 0){
+    console.log(number_2,'is positive')
+}else if(number_2 < 0){
+    console.log(number_2,'is negative')
+}
