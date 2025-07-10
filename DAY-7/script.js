db08a9ddbec24db08c5115afe113a6594010eed7
Ajinath-Game <gameajinath4@gmail.com>
2025-07-10 19:31:10 +0530

    loops DAY-7

---
+++ DAY-7/script.js
@@ -0,0 +1,61 @@
+
+
+// looping : looping is use to print and retrive a multiple data
+
+
+// 1 While loop
+
+let no = 0
+
+
+while(no <= 10){
+    console.log(no)
+    no++
+}
+
+
+// print 0 t0 100 even number on console
+
+let count = 0
+
+while(count <= 100){
+    console.log(count)
+    count = count + 2
+}
+
+
+// print 0 t0 100 odd number on console
+
+let count1 = 1
+
+while(count1 <= 100){
+    console.log(count1)
+    count1= count1 + 2
+}
+
+
+
+// 2 dowhile loop
+
+
+let number = 0
+
+do{
+    console.log(number)
+    number++
+}while(number <= 10);
+
+
+// print 0 t0 100 even number on console
+
+let count2 = 0
+
+
+do{
+    console.log(count2)
+    count2 = count2 + 2
+}while(count2 <= 100);
+
+
+
+
