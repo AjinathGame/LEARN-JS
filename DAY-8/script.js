8669f3922d0882de047fb65f65594dbfe8d1849d
Ajinath-Game <gameajinath4@gmail.com>
2025-07-11 19:16:32 +0530

    for loop DAY8

---
+++ DAY-8/script.js
@@ -0,0 +1,36 @@
+// for loop
+
+// write a program to create a table
+let no = 6
+
+for (let i = 1; i <= 10 ; i++){
+    console.log(no * i)
+
+}
+
+
+
+// write a program to print sum of 10 numbers
+
+
+let numbers = 1
+
+let sum = 0
+
+for(numbers ; numbers <= 10 ; numbers++){
+    sum = sum + numbers
+}
+console.log(sum)
+
+
+
+for(let i = 0 ; i < 5 ; i++){
+    for(let j = 0 ; j< i; j++){
+        console.log('*')
+    
+    }
+    console.log('\n')
+
+    
+
+}
