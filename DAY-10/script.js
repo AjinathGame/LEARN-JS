109a1e9809c1deafc6360740f9b6705d6a0819a8
Ajinath-Game <gameajinath4@gmail.com>
2025-07-16 20:41:43 +0530

    array DAY10

---
+++ DAY-10/script.js
@@ -0,0 +1,34 @@
+
+
+
+// Array 
+
+let a = [1,2,3,4,5,6,7,8,9,10]
+
+console.log(a)
+
+
+
+
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+
+
+console.log(a.length)   // find length of array
+
+
+
+let colors = ['red','green','blue','yellow','black','white','pink','orange',]
+
+for(let i=0;i<colors.length;i++){
+    console.log(colors[i])
+}
+
+
+
+let days = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
+
+for(let i=0;i<days.length;i++){
+    console.log(days[i])
+}
