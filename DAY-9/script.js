4b6c5d7f693f77a0ebada9cba7960d959f532290
Ajinath-Game <gameajinath4@gmail.com>
2025-07-16 20:29:10 +0530

    Document in javascript DAY9

---
+++ DAY-9/script.js
@@ -0,0 +1,44 @@
+
+
+// Document in javascript
+
+
+console.log(document)    // document mean html file , it is show all html content on console
+
+
+let title = document.title   // document.title show title of file
+
+
+console.log(title)
+
+
+
+let titles = document.title = " we learn javascript"    // we can change file title 
+
+console.log(titles)
+
+
+
+let name = document.writeln('hello world') // document.write is used to write anything on output
+
+
+let no = 1234
+
+document.write("<h1>" + no +" </h1> ")
+
+
+let names = 'ajinath'
+
+document.writeln(names + ' welcome')
+
+
+for(let i = 1 ; i<=10 ; i++)
+{
+    let number = 2;
+     number = number * i;
+    document.writeln(number + "<br>")
+}
+
+
+
+
