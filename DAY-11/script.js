519df766ed67f45f26372fdaaeb126b211304f31
Ajinath-Game <gameajinath4@gmail.com>
2025-07-16 21:05:07 +0530

    shift , unshift , push , pop , splice functions DAY11

---
+++ DAY-11/script.js
@@ -0,0 +1,50 @@
+
+// shift and unshift function
+
+
+let color = ['red','pink','blue','green','yellow']
+
+color.unshift('pink')   //this function is add data in starting end
+
+console.log(color)
+
+let day = ['mon','tue','wed','thu','fri']
+
+day.shift()      //this function is  remove data in starting end
+
+console.log(day)    
+
+
+
+// push and pop function
+
+
+let animal =['dog','cat','mouse','tiger']
+
+
+animal.push('horse')    //this function is add data in last
+
+console.log(animal)
+
+
+let fruit = ['apple','mango', 'banana']
+
+fruit.pop()     //this function is remove data in last
+
+console.log(fruit)
+
+// splice function 
+
+// syntax :  month.splice(index , count , item1 , item2 .....)
+
+let month = ['jan','feb','march','april','may']
+
+month.splice(2,0,'dec')
+
+console.log(month)
+
+
+
+let days= ['sun','mon','tue','wed','thu','fri','sat']
+
+month.splice(2,2)
