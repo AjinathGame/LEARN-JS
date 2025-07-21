6d0c51384f253728a9c89b750247aaa968873f99
Ajinath-Game <gameajinath4@gmail.com>
2025-07-21 18:47:18 +0530

    function in javascript DAY14

---
+++ DAY-14/script.js
@@ -0,0 +1,62 @@
+
+//  eg., 1
+
+function easycode(){
+    console.log("hello");
+}
+
+easycode()
+
+//   eg., 2
+
+function ajinath(){
+    console.log("hii");
+}
+
+ajinath()
+
+// function definition part
+
+function js(name){
+    console.log(`${name}`)
+}
+
+// function call
+
+js("hii")
+js("hello")
+js("welcome")
+
+js()
+
+
+
+
+
+// eg., 5
+
+function sayHello(name="user"){
+    console.log("hello")
+}
+
+sayHello("ajinath")
+
+
+//  eg., 6
+
+function sayGM(name="please enter your name"){
+    console.log(`good mrning ${name}`)
+}
+
+sayGM("hello")
+
+
+// eg., 7
+
+function sum(num1,num2){
+    console.log(num1+num2)
+}
+
+sum(19,7)
+
+
