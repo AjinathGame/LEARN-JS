8cab0f58ec4ba22530a5a27666bf66c7d1e82101
Ajinath-Game <gameajinath4@gmail.com>
2025-07-23 19:06:32 +0530

    DOM DAY16

---
+++ DAY-16/script.js
@@ -0,0 +1,18 @@
+
+//  DOM (document object model)
+
+// 1 
+
+
+
+let res = document.getElementsByTagName("p")
+
+console.log(res)
+
+
+
+// 2
+
+
+let result = document.getElementsByClassName("hii")
+console.log(result)
