b94379e4a8a076170d7d04b9967a2cc8913b4b0e
Ajinath-Game <gameajinath4@gmail.com>
2025-07-24 19:41:50 +0530

    DOM DAY17

---
+++ DAY-17/script.js
@@ -0,0 +1,33 @@
+
+
+// 3 
+
+
+let res = document.getElementById("hii");
+
+console.log(hii)
+
+
+// 4
+
+
+let result = document.querySelector("#hii")
+
+// innerText
+
+res.innerText="hello"
+
+console.log(res)
+
+// innerHtml
+
+result.innerHTML="<h1>hii</h1>"
+
+
+// change style
+
+res.style.color="red"
+res.style.backgroundColor="yellow"
+
+
+
